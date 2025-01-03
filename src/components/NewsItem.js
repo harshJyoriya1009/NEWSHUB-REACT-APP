c92de16fbfc027af7f80881b9c501994b5540007
Harsh Jyoriya <harshjyoriyagdg24@gmail.com>
2025-01-04 01:03:13 +0530

    Again some new features

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,10 +7,7 @@ export class NewsItem extends Component {
       <div>
         <div className="card" style={{ width: "18rem;" }}>
           <img
-            src="https://ichef.bbci.co.uk/ace/branded_news/1200/cpsprodpb/2c12/live/db121a50-c92e-11ef-87df-d575b9a434a4.jpg"
-            className="card-img-top"
-            alt="..."
-          />
+            src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg" className="card-img-top" />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
