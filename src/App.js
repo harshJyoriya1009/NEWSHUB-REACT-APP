f6019d8f97788a1999c86cb56f86cd4b1c42feaf
Harsh Jyoriya <harshjyoriyagdg24@gmail.com>
2025-01-06 17:17:50 +0530

    Fun

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
 
-
-
-
-
 export default class App extends Component {
   c="Harsh"
   render() {
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
 
-
   render() {
     let { title, description, imageUrl, newsUrl } = this.props;
     return (
