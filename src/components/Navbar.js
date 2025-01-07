927e8c16f9b8973e37d4d6a9f2c7b9b2a71e3dec
Harsh Jyoriya <harshjyoriyagdg24@gmail.com>
2025-01-07 23:21:44 +0530

    FUN

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { Link} from 'react-router-dom';
                                        
 export class Navbar extends Component {
   render() {
@@ -16,15 +15,15 @@ export class Navbar extends Component {
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
 
-                        <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/">Home</Link></li>
-                        <li className="nav-item"><Link className='nav-link active' aria-current='page' to="/about">About</Link></li>
-                        <li className="nav-item"><Link className='nav-link active' aria-current='page' to="/about">Business</Link></li>
-                        <li className="nav-item"><Link className='nav-link active' aria-current='page' to="/about">Entertainment</Link></li>
-                        <li className="nav-item"><Link className='nav-link active' aria-current='page' to="/about">General</Link></li>
-                        <li className="nav-item"><Link className='nav-link active' aria-current='page' to="/about">Health</Link></li>
-                        <li className="nav-item"><Link className='nav-link active' aria-current='page' to="/about">Science</Link></li>
-                        <li className="nav-item"><Link className='nav-link active' aria-current='page' to="/about">Sports</Link></li>
-                        <li className="nav-item"><Link className='nav-link active' aria-current='page' to="/about">Technology</Link></li>
+                        <li className="nav-item"><a className="nav-link active" aria-current="page" href="/">Home</a></li>
+                        <li className="nav-item"><a className='nav-link active' aria-current='page' href="/about">About</a></li>
+                        <li className="nav-item"><a className='nav-link active' aria-current='page' href="/about">Business</a></li>
+                        <li className="nav-item"><a className='nav-link active' aria-current='page' href="/about">Entertainment</a></li>
+                        <li className="nav-item"><a className='nav-link active' aria-current='page' href="/about">General</a></li>
+                        <li className="nav-item"><a className='nav-link active' aria-current='page' href="/about">Health</a></li>
+                        <li className="nav-item"><a className='nav-link active' aria-current='page' href="/about">Science</a></li>
+                        <li className="nav-item"><a className='nav-link active' aria-current='page' href="/about">Sports</a></li>
+                        <li className="nav-item"><a className='nav-link active' aria-current='page' href="/about">Technology</a></li>
 
 
                         </ul> 
