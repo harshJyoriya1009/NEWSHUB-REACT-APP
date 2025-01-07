1feac4476da5481c59eddb5ae5a6ae089e4c3203
Harsh Jyoriya <harshjyoriyagdg24@gmail.com>
2025-01-07 23:10:37 +0530

    FUN

--- src/App.js
+++ src/App.js
@@ -3,15 +3,35 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 export default class App extends Component {
   c="Harsh"
   render() {
     return (
+      <>
       <div>
         <Navbar/>
         <News pageSize={6} country="us" category="health"/>
       </div>
+
+
+{/* <Router>
+        <div>
+          Navbar is always visible regardless of the route
+          <Navbar />
+          <Routes>
+            Define the Route for the News component
+            <Route path="/" element={<News pageSize={6} country="us" category="health" />} />
+          </Routes>
+        </div>
+      </Router> */}
+      </>
     )
   }
 }
